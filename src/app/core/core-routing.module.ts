8b733cacfb5c59cb055d5813110b69a9df44ab8b
code-knayam <mayankjain1394@gmail.com>
2020-04-19 16:33:18 +0530

    structural and routing changes

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { FeedComponent } from "./feed/feed.component";
 import { AddPostComponent } from "./add-post/add-post.component";
-import { ProfileComponent } from "./profile/profile.component";
+import { ProfileComponent } from "./shared/profile/profile.component";
 import { CoreComponent } from "./core.component";
+import { ViewPostComponent } from "./view-post/view-post.component";
+import { MyProfileComponent } from "./my-profile/my-profile.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 const routes: Routes = [
   {
     path: "",
     component: CoreComponent,
     children: [
-      { path: "", component: FeedComponent },
+      { path: "feed", component: FeedComponent },
       { path: "add", component: AddPostComponent },
-      { path: "profile", component: ProfileComponent },
+      { path: "profile", component: MyProfileComponent },
+      { path: "user/:id", component: UserProfileComponent },
+      { path: "post/:id", component: ViewPostComponent },
+      { path: "", redirectTo: "feed", pathMatch: "full" },
     ],
   },
 ];
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CoreComponent } from "./core.component";
 import { FeedComponent } from "./feed/feed.component";
-import { ProfileComponent } from "./profile/profile.component";
+import { ProfileComponent } from "./shared/profile/profile.component";
 import { AddPostComponent } from "./add-post/add-post.component";
 import { RouterModule } from "@angular/router";
 import { CoreRoutingModule } from "./core-routing.module";
-import { PostComponent } from "./post/post.component";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { PostComponent } from "./shared/post/post.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { ViewPostComponent } from "./view-post/view-post.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { MyProfileComponent } from "./my-profile/my-profile.component";
 
 @NgModule({
   declarations: [
@@ -18,6 +21,9 @@ import { AngularFontAwesomeModule } from "angular-font-awesome";
     AddPostComponent,
     NavbarComponent,
     PostComponent,
+    ViewPostComponent,
+    UserProfileComponent,
+    MyProfileComponent,
   ],
   imports: [
     CommonModule,
